fbe72ab776b1c0c5897cf2635df1c54708aff4af
ayaarragab <ayarsk4343@gmail.com>
2024-10-06 18:13:48 +0300

    created category CRUD functionalities

---
+++ controllers/CategoryController.js
@@ -0,0 +1,133 @@
+import Category from "../models/category";
+import serverErrorsHandler from "./helper.js";
+
+export default class CategoryController {
+    static async createCateogry(request, response) {
+        try {
+            const { name } = request.params;
+            const user_id = request.user._id;
+            const category = await Category.create({
+                name,
+                user_id
+            });
+            response.status(200).json({
+                "status": "success",
+                "message": `You've created the category ${category.name} in successfully`,
+                "data": category
+            })
+        } catch (error) {
+            serverErrorsHandler(response, error)
+        }
+    }
+
+    static async retrieveCategories(request, response) {
+       const user_id = request.user._id;
+       try {
+        const categories = await Category.find({user_id});
+        if (!categories) {
+            response.status(404).json({
+                "status": "error",
+                "message": "You've not created any categories yet!",
+                "error": {
+                    "code": 404,
+                }
+            })
+        }
+        response.status(200).json({
+            "status": "success",
+            "message": `Here's all your categories`,
+            "data": categories
+        })
+       } catch (error) {
+        serverErrorsHandler(response, error);
+       }
+    }
+
+    static async updateCateogry(request, response) {
+        try {
+            const { cateogryId } = request.params;
+            const updateData = request.body;
+    
+            if (!cateogryId) {
+                return response.status(400).json({
+                    "status": "error",
+                    "message": "An error occurred.",
+                    "error": {
+                        "code": 400,
+                        "details": "Cateogry ID is required."
+                    }
+                });
+            }
+    
+            const updatedCat = await Category.findByIdAndUpdate(
+                cateogryId,
+                updateData,
+                { new: true }
+            );
+    
+            if (!updatedCat) {
+                return response.status(404).json({
+                    "status": "error",
+                    "message": "An error occurred.",
+                    "error": {
+                        "code": 404,
+                        "details": "Category not found."
+                    }
+                });
+            }
+    
+            response.status(200).json({
+                "status": "success",
+                "message": "Category updated successfully.",
+                "data": updatedCat
+            });
+    
+        } catch (error) {
+            serverErrorsHandler(response, error);
+        }
+    }
+
+    static async deleteCateogry(request, response) {
+        try {
+            const { cateogryId } = request.params;
+    
+            if (!cateogryId) {
+                return response.status(400).json({
+                    "status": "error",
+                    "message": "An error occurred.",
+                    "error": {
+                        "code": 400,
+                        "details": "Cateogry ID is required."
+                    }
+                });
+            }
+            try {
+                await Category.findByIdAndDelete(cateogryId);
+                response.status(200).json({
+                    "status": "success",
+                    "message": "Category deleted successfully.",
+                    "data": []
+                });
+            } catch (error) {
+                serverErrorsHandler(response, error);
+            }
+        } catch (error) {
+            serverErrorsHandler(response, error);
+        }      
+    }
+
+    static async deleteAllCategories(request, response) {
+        try {
+            const userId = request.user._id;
+    
+            const result = await Category.deleteMany({ user_id: userId });
+    
+            response.status(200).json({
+                status: "success",
+                message: `${result.deletedCount} categories deleted successfully.`,
+            });
+        } catch (error) {
+            serverErrorsHandler(response, error);
+        }
+    }
+}
--- models/category.js
+++ models/category.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const categoryModel = mongoose.Schema({
     name: { type: String, required: true },
-    thoughts: [{ type: mongoose.Types.ObjectId, ref: 'Thought' }],
+    thoughts: [{ type: mongoose.Types.ObjectId, ref: 'Thought', required: false }],
+    user_id: { type: mongoose.Types.ObjectId },
     created_at: { type: Date, default: Date.now }
 });
 
