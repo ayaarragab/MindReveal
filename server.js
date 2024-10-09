9c58eca070a943373dee85c791f7c0ce869be8d0
ayaarragab <ayarsk4343@gmail.com>
2024-10-09 16:14:35 +0300

    server.js docs

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+/**
+ * @file server.js
+ * @brief Main entry point for the MindReveal API application.
+ * 
+ * This file initializes the Express application, configures middleware,
+ * establishes a database connection, and sets up API routes.
+ * 
+ * @note Ensure that the environment variable PORT is set before running the server.
+ */
+
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import DBClient from "./config/db.js";
 import router from "./routes/routes.js";
-import category from "./models/category.js";
 
 await DBClient.getConnection();
 
